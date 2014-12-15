80b6968c10e57bc73c3208245323b7ddc8204b57
Martin Biermann <info@martinbiermann.com>
2014-12-15 14:43:27 +0100

    Adds nginx provisioning

--- deployment/provision.sh
+++ deployment/provision.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
-# Install Git
-sudo apt-get install git -y
+
 # Install NodeJS
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get install -y nodejs
-cd -
+
 # Install upstart script
 sudo mkdir -p /var/log/intdai-mock-api
-sudo cp intdai-mock-api.conf /etc/init
+sudo cp /opt/intdai-mock-api/deployment/intdai-mock-api.conf /etc/init
+
 # Install app dependencies
-cd ../
 npm install
+
 # Start app
 sudo service intdai-mock-api restart
+
+# Install nginx
+sudo apt-get update
+sudo apt-get install nginx -y
+
+# Override default nginx configuration
+sudo cp /opt/intdai-mock-api/deployment/intdai-mock-api.nginx.conf /etc/nginx/sites-available/default
+
+# Restart nginx to populate latest configuration changed
+sudo service nginx restart
