2018a8a20371dfa3483d0145eb133e9621d54838
Martin Biermann <info@martinbiermann.com>
2014-12-15 11:13:43 +0100

    Fixes order of commands in provisioning script

--- deployment/provision.sh
+++ deployment/provision.sh
@@ -8,8 +8,8 @@ cd -
 # Install upstart script
 sudo mkdir -p /var/log/intdai-mock-api
 sudo cp intdai-mock-api.conf /etc/init
-# Start app
-sudo service intdai-mock-api restart
 # Install app dependencies
 cd ../
 npm install
+# Start app
+sudo service intdai-mock-api restart
